e442b0a4c5dd0c71a9af60f38185c6ff77c461dc
郑瑞琦 <23201111@stu.nchu.edu.cn>
2026-06-12 23:06:32 +0800

    chore: cleanup WSL temp scripts, update .gitignore, restore Windows deps

--- wsl_install_electron.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export PATH=/tmp/node-v18.20.4-linux-x64/bin:/usr/bin:/bin
-export ELECTRON_MIRROR=https://npmmirror.com/mirrors/electron/
-cd /mnt/d/BaoFlashBrowser
-node node_modules/electron/install.js 2>&1
-echo "INSTALL_EXIT=$?"
--- wsl_setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export PATH=/tmp/node-v18.20.4-linux-x64/bin:/usr/bin:/bin
-cd /mnt/d/BaoFlashBrowser
-rm -rf node_modules package-lock.json
-npm install 2>&1
